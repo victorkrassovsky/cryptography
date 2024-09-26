f64c7a86deccd10556c830133f2356585adc9cf7
Victor Krassovsky <victorkrassovsky@hotmail.com>
2024-09-25 23:53:08 -0700

    fixed ctr mode

--- ctr.py
+++ ctr.py
@@ -15,26 +15,25 @@ def array_to_string(arr):
 
 #takes any string, 16 byte key and an optional nonce, and encrypts using aes128 ctr mode
 #returns a string of bytes corresponding to the cipher text
-#note: if the end of the plaintext ends in \x00, it will be removed due to padding
 def aes_128_ctr_encrypt(plaintext, key, nonce=os.urandom(8)):
     ct_array = None*(len(plaintext)/8)
     for i in range(0,len(pt_array)):
         counter = i.to_bytes(8,'big')
-        block = aes128.encrypt(nonce + counter)
+        block = aes128.encrypt(nonce + counter,key)
         ct_array[i] = xor_blocks(block,pt_array[i])
-    return nonce + array_to_string(ct_array)
+    return nonce + bytes(8) + array_to_string(ct_array)
 
 
 #takes a string of bytes returned from encrypt method and returns corresponding plaintext
 def aes_128_ctr_decrypt(ct, key):
-    nonce = ct[0:8]
-    ct = ct[8:]
+    nonce = ct[0:16]
+    ct = ct[16:]
     ct_array = [ct[i:i+16] for i in range(0,len(ct), 16)]
     pt_array = [None]*len(ct_array)
     for i in range(0,len(pt_array)):
-        counter = i.to_bytes(8,'big')
-        pt_array[i] = xor_blocks(aes128.encrypt(nonce + counter,key),ct_array[i])
-    pt = array_to_string(pt_array)
-    return pt
+        counter = (int.from_bytes(nonce[8:16],'big') + i).to_bytes(8,'big')
+        block = aes128.encrypt(nonce[0:8]+counter,key);
+        pt_array[i] = xor_blocks(block,ct_array[i]);
+    return b''.join(pt_array)
 
 
