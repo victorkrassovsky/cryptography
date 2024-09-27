e0ff749afec2f1f470ac285866511027a8400a36
Victor Krassovsky <victorkrassovsky@hotmail.com>
2024-09-27 14:23:42 -0700

    wrote more

--- sha-2.py
+++ sha-2.py
@@ -9,8 +9,12 @@ def pad_to_512(b):
 def xor_words(b1,b2):
     return b''.joint([(x^y).to_bytes(1,'big') for x,y in zip(b1,b2)])
 
+#xors three 4 byte words
+def xor_three_word(b1,b2,b3):
+    return xor_word(b1,xor_words(b2,b3))
+
 # right rotates a 4 byte word n times
-def right_rotate_word(word, n):
+def right_rotate_words(word, n):
     b = int.from_bytes(word,'big')
     n = n%32
     new_word = (b >> n) ^ ((b%(1<<n)) << (32-n))
@@ -29,7 +33,7 @@ def sha256 (b):
     
     h = "6a09e667 bb67ae85 3c6ef372 a54ff53a 510e527f 9b05688c 1f83d9ab 5be0cd19".split(' ')
     h = [bytes.fromhex(x) for x in h]
-    k="""428a2f98 71374491 b5c0fbcf e9b5dba5 3956c25b 59f111f1 923f82a4 ab1c5ed5
+    k ="""428a2f98 71374491 b5c0fbcf e9b5dba5 3956c25b 59f111f1 923f82a4 ab1c5ed5
     d807aa98 12835b01 243185be 550c7dc3 72be5d74 80deb1fe 9bdc06a7 c19bf174
     e49b69c1 efbe4786 0fc19dc6 240ca1cc 2de92c6f 4a7484aa 5cb0a9dc 76f988da
     983e5152 a831c66d b00327c8 bf597fc7 c6e00bf3 d5a79147 06ca6351 14292967
@@ -40,6 +44,10 @@ def sha256 (b):
     k = [bytes.fromhex(x) for x in k]
     
     for block in chunks:
-        s = 0
-        
-    
+        w = [bytes(4)]*64
+        for i in range(16):
+            w[i] = block[4*i:4*i+4]
+        for i in range(16, 64):
+            s0 = xor_three_words(right_rotate_word(w[i-15],7), right_rotate_word(w[i-15],18), right_shift_word(w[i-15],3))
+            s1 = xor_three_words(right_rotate_word(w[i-2], 17), right_rotate_word(w[i-2],19), right_shift_word(w[i-2],10))
+            w[i] = (int.from_bytes(w[i-16], 'big') + int.from_bytes(s0,'big') + int.from_bytes(w[i-7],'big') + int.from_bytes(s1,'big')).to_bytes(4,'big')
